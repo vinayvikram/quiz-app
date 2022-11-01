3b75757860e8759fe2f76e8060016652874a6c2a
Vinay Vikram <vinayandvikram@gmail.com>
2022-11-01 21:45:28 +0530

    Update FileUploader.js

--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -1,5 +1,46 @@
 import React, { useState } from "react";
 
+const sampleJSON = [
+  {
+    "question": "India is {} largest country by population",
+    "type": "FITB1",
+    "time": 20,
+    "answers": ["second"]
+  },
+  {
+    "question": "India is {} largest country by population and ranks {} in terms of landmass",
+    "type": "FITB2",
+    "time": 20,
+    "answers": ["second", "seventh"]
+  },
+  {
+    "question": "Select top two countries which has highest GDP",
+    "type": "MAQ",
+    "time": 20,
+    "answer_choices": ["USA", "China", "Japan", "Germany", "UK", "India"],
+    "answers": ["USA", "China"]
+  },
+  {
+    "question": "Which among the following is the most valuable company in the world",
+    "type": "MCQ",
+    "time": 20,
+    "answer_choices": ["Tesla", "Apple", "Microsoft", "Netflix", "Google"],
+    "answer": "Tesla"
+  },
+  {
+    "question": "Match the following captials with states",
+    "type": "MTF",
+    "time": 20,
+    "answer_options": [
+      { "State": "Maharashtra", "Capital": "Mumbai" },
+      { "State": "Punjab", "Capital": "Chandigarh" },
+      { "State": "Karnataka", "Capital": "Bengaluru" },
+      { "State": "Himachal Pradesh", "Capital": "Shimla" },
+      { "State": "Rajasthan", "Capital": "Jaipur" }
+    ]
+  }
+]
+
 const FileUploader = ({ setQuestions, setTotalTime, setIndex }) => {
   const [isUploaded, setIsUploaded] = useState(false);
   const [isSelected, setIsSelected] = useState(false);
@@ -40,7 +81,9 @@ const FileUploader = ({ setQuestions, setTotalTime, setIndex }) => {
         }, totalTime * 1000);
         document.getElementById("overlay").style.display = "none";
       })
-      .catch((error) => setIsError(true));
+      .catch((error) => {
+             setIsError(true);
+    });
   };
 
   return (
@@ -58,6 +101,11 @@ const FileUploader = ({ setQuestions, setTotalTime, setIndex }) => {
           disabled={isUploaded && !isError ? false : true}
         >
           Start Quiz
+        </button>
+           <button
+          onClick={() => setQuestions(sampleJSON)}
+           >
+          Start Demo
         </button>
       </div>
     </div>
